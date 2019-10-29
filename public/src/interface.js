e96097230771776c0a35d318c497b208b159eaae
Student <john.dennehy@gmail.com>
2019-10-29 16:11:00 +0000

    removes pointless recreation of existing js functions

--- public/src/interface.js
+++ public/src/interface.js
@@ -6,26 +6,8 @@ function listenFor(element, action, callBack, callBackParameter) {
 });
 }
 
-function elementByID(id) {
-  return document.querySelector("#" + id);
-}
-
-function elementByClass(className) {
-  return document.querySelector("." + className);
-}
-
-function elementByTag(tag) {
-  return document.querySelector(tag);
-}
-
-function getElement(element) {
-  return document.querySelector(element);
-}
-
 document.addEventListener('DOMContentLoaded', function () {
   console.log( "I'm listening" );
 
-  listenFor(elementByID('create_button'), 'click', alert,'hello world' )
-
+  listenFor(document.getElementById('create_button'), 'click', alert,'hello world' )
 });
-
