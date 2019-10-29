a8d5aa924f6e179d66ba1e64572ecca2fed8fa4c
Student <john.dennehy@gmail.com>
2019-10-29 17:10:44 +0000

    Adds function to create new note html within index upen clicking button with id "create_button"

--- public/src/interface.js
+++ public/src/interface.js
@@ -1,28 +1,34 @@
 "use strict";
 
-function listenFor(element, action, callBack, callBackParameter) {
-  element.addEventListener(action, function() {
-  callBack(callBackParameter)
-});
+function listenFor(element, action, callBack, callBackParameter = null) {
+  element.addEventListener(action, function () {
+    callBack(callBackParameter)
+  });
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-  console.log( "I'm listening" );
-
-  listenFor(document.getElementById('create_button'), 'click', alert,'hello world' )
+  console.log("I'm listening");
+  listenFor(document.getElementById('create_button'), 'click', addToList)
 });
 
-var notes = ["hello", "Bye", "smelly", "cool", "monkeys"]
-
-var list = function listNotes() {
-  var list = document.getElementById('populate_notes')
-  console.log(notes)
-  notes.forEach(function(note){
-    var listNote = document.createElement('li');
-    listNote.appendChild(document.createTextNode(note));
-    list.appendChild(listNote);
-  })
+function getNote() {
+  var text = document.getElementById('note_content').value
+  return text
 }
 
-list()
+function clearNoteBox() {
+  document.getElementById('note_content').value = ""
+}
 
+function addToList() {
+  var note = getNote()
+  var list = document.getElementById('populate_notes')
+  list.appendChild(createNoteHTML(note));
+  clearNoteBox()
+}
+
+function createNoteHTML(note) {
+  var htmlElement = document.createElement('li');
+  htmlElement.appendChild(document.createTextNode(note));
+  return htmlElement
+}
