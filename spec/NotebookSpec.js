aab9c155d69cb93a1b9120a232c26a49d63ba45b
kevindhanna <kevindhanna@gmail.com>
2019-10-30 14:12:36 +0000

    refactor to module and add get function

--- spec/NotebookSpec.js
+++ spec/NotebookSpec.js
@@ -2,17 +2,27 @@ Describe("Notebook", function(){
 
   Describe('all', function(){
     It('returns all notes', function() {
-      notes2 = Notebook
-      notes2.add("Hello")
-      Demand("Hello").toBeAVassalOf(notes2.all())
+      notes = new Notebook
+      notes.add("Hello")
+      Demand("Hello").toBeAVassalOf(notes.all())
     });
   });
 
   Describe('add', function(){
     It('creates a new note', function() {
-      var notes = Notebook
+      var notes = new Notebook
       notes.add("Hello World");
+      
       Demand("Hello World").toBeAVassalOf(notes.all())
     });
   });
+  
+  Describe('get', function() {
+    It('gets the note at a given index', function() {
+      var notes = new Notebook
+      notes.add("Hello World");
+
+      Demand(notes.get(0)).toBe('Hello World')
+    })
+  })
 });
